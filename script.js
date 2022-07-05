6a0ebdf839d9dec5879be3a29bb1ccd644fee723
DM-Malkoff <dm.malkoff@gmail.com>
2022-07-05 12:36:12 +0400

    fixes

--- script.js
+++ script.js
@@ -3,14 +3,9 @@ let textBlock = document.querySelectorAll(".spoiler-block__item .text");
 for (let i = 0; i < spoiler.length; i++) {
     spoiler[i].addEventListener('click', function () {
         spoiler[i].classList.toggle('active')
-        console.log(this.classList)
-        for (let j = 0; j < textBlock.length; j++) {
-            textBlock[i].style.height = spoiler[i].classList.contains('active') ? textBlock[i].scrollHeight + "px" : 0
-        }
+        textBlock[i].style.height = spoiler[i].classList.contains('active') ? textBlock[i].scrollHeight + "px" : 0
     })
 }
 for (let i = 0; i < spoiler.length; i++) {
-    for (let j = 0; j < textBlock.length; j++) {
-        textBlock[i].style.height = spoiler[i].classList.contains('active') ? textBlock[i].scrollHeight + "px" : 0
-    }
+    textBlock[i].style.height = spoiler[i].classList.contains('active') ? textBlock[i].scrollHeight + "px" : 0
 }
